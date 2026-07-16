82399c94971563dadfdd052c869b7724e472dd0f
Mohiuuddin <shagorinnocent24@gmail.com>
2026-07-16 16:34:57 +0600

    feat: add groq loan analysis service

--- src/services/ai/generate-loan-analysis.ts
+++ src/services/ai/generate-loan-analysis.ts
@@ -1,5 +1,66 @@
-import Groq from "groq-sdk";
+import { groq } from "@/lib/groq";
+import { EmploymentType } from "@/generated/prisma/enums";
 
-export const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY!,
-});
+interface LoanAnalysisInput {
+  loanAmount: number;
+  monthlyIncome: number;
+  monthlyExpenses: number;
+  existingLoanEmi: number;
+  savings: number;
+  creditScore: number;
+  employmentType: EmploymentType;
+}
+
+export async function generateLoanAnalysis(input: LoanAnalysisInput) {
+  const prompt = `
+You are a senior loan underwriter.
+
+Analyze the following loan application.
+
+Return ONLY valid JSON.
+
+{
+  "eligible": true,
+  "riskScore": 0,
+  "confidenceScore": 0,
+  "recommendedAmount": 0,
+  "reasoning": ""
+}
+
+Loan Details
+
+Requested Amount: ${input.loanAmount}
+
+Employment Type: ${input.employmentType}
+
+Monthly Income: ${input.monthlyIncome}
+
+Monthly Expenses: ${input.monthlyExpenses}
+
+Existing EMI: ${input.existingLoanEmi}
+
+Savings: ${input.savings}
+
+Credit Score: ${input.creditScore}
+`;
+
+  const response = await groq.chat.completions.create({
+    model: process.env.GROQ_MODEL!,
+    temperature: 0.2,
+    response_format: {
+      type: "json_object",
+    },
+    messages: [
+      {
+        role: "system",
+        content: "You are an expert loan underwriting AI. Return only JSON.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  return JSON.parse(response.choices[0].message.content!);
+}
